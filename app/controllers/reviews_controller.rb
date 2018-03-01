068a7fe365ced6dcb60a3e11427ae55afd0c74be
Raghu Betina <raghu@firstdraft.com>
2018-03-01 15:05:54 +0000

    Add forms for new collection objects on show pages

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -16,6 +16,8 @@ class ReviewsController < ApplicationController
   end
 
   def show
+    @response = Response.new
+    @pic = Pic.new
     @review = Review.find(params[:id])
 
     render("reviews/show.html.erb")
